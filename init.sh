de4b5cda962d38640cecaa6197f3e1e7bd28fa94
digital-shokunin <david.mitchell@digital-shokunin.net>
2015-03-10 16:42:18 -0400

    Fix ruby

--- init.sh
+++ init.sh
@@ -2,17 +2,14 @@
 
 
 curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-curl -o rvm.sh -L get.rvm.io && cat rvm.sh | bash -s stable --autolibs=enabled --ruby=2.1.5
-source /usr/local/rvm/src/rvm/scripts/rvm
-touch /.bashrc
+curl -o rvm.sh -sSL get.rvm.io && cat rvm.sh | bash -s stable --autolibs=enabled --ruby=2.1.5
+source /usr/local/rvm/scripts/rvm
 touch /root/.bashrc
-echo 'source /usr/local/rvm/src/rvm/scripts/rvm' >> /root/.bashrc
-echo 'source /usr/local/rvm/src/rvm/scripts/rvm' >> /.bashrc
+echo 'source /usr/local/rvm/scripts/rvm' >> /root/.bashrc
 source /root/.bashrc
-/usr/local/rvm/bin/rvm install 2.1.5
 /usr/local/rvm/bin/rvm use 2.1.5 --default
 
-
+source /usr/local/rvm/scripts/rvm
 # /etc/init.d/postgresql start
 /usr/bin/msfupdate
 /usr/bin/msfconsole	
