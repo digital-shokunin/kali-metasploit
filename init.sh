0d795c31f46e8ece3b061027d2f7d84abf515c15
digital-shokunin <david.mitchell@digital-shokunin.net>
2015-03-10 16:37:56 -0400

    Ruby install moved to init

--- init.sh
+++ init.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -o rvm.sh -L get.rvm.io && cat rvm.sh | bash -s stable --autolibs=enabled --ruby=2.1.5
+source /usr/local/rvm/src/rvm/scripts/rvm
+touch /.bashrc
+touch /root/.bashrc
+echo 'source /usr/local/rvm/src/rvm/scripts/rvm' >> /root/.bashrc
+echo 'source /usr/local/rvm/src/rvm/scripts/rvm' >> /.bashrc
+source /root/.bashrc
+/usr/local/rvm/bin/rvm install 2.1.5
+/usr/local/rvm/bin/rvm use 2.1.5 --default
+
+
 # /etc/init.d/postgresql start
 /usr/bin/msfupdate
 /usr/bin/msfconsole	
