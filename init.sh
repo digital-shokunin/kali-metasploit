cdb5a4f493a719fb480d5dd8a08ba876a7a7d59e
digital-shokunin <david.mitchell@digital-shokunin.net>
2015-03-10 16:43:17 -0400

    Fix ruby

--- init.sh
+++ init.sh
@@ -7,9 +7,9 @@ source /usr/local/rvm/scripts/rvm
 touch /root/.bashrc
 echo 'source /usr/local/rvm/scripts/rvm' >> /root/.bashrc
 source /root/.bashrc
-/usr/local/rvm/bin/rvm use 2.1.5 --default
 
-source /usr/local/rvm/scripts/rvm
+
+
 # /etc/init.d/postgresql start
 /usr/bin/msfupdate
 /usr/bin/msfconsole	
